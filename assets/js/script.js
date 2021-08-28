5e6900c4a6dd3d8f6a20e95b3c17622349757434
ilkkantuluce07 <60572381+ilkkantuluce07@users.noreply.github.com>
2021-08-28 18:40:44 +0300

    home page almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
+$('.single-item-wrap').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [{
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+          }
+      }, {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      }, {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+
+  ]
+});
+
 $(document).ready(function(){
     $(".dropdown-toggle").click(function(){
       $(".menu-dropdown-bg").toggleClass("show");
